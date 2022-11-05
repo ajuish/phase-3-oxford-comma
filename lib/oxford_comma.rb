9ec7f09434c2072e53f5324d09bdce01d974471f
Alex Ju <aju217@gmail.com>
2022-11-05 11:53:10 -0400

    commit tree 0f635d028d8c999a8ed63e9b488aa13acaef8204

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    # if (array.length == 1)
+    #     array[0]
+    # elsif (array.length == 2)
+    #     array.join(" and ")
+    # else
 
+    #     array[0...-1].join(", ") + ", and " + array[-1]
+    # end
+
+    return array.join(" and ") if array.size < 3
+
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ") 
 end
